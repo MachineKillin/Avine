b86d2dc9514d2cc2d4b7d9b0d02f52ab1c6b3291
Killin <56980137+MachineKillin@users.noreply.github.com>
2023-03-16 08:51:37 -0400

    Update avine.py
    cleaned up code slightly

--- avine.py
+++ avine.py
@@ -84,35 +84,6 @@ badlinks = [
 'https://papago.naver.net',
 'https://papago.naver'
 ] #filteres any likes with these in it
-headerzz = [
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0) Gecko/20190101 Firefox/77.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (X11; Linux ppc64le; rv:75.0) Gecko/20100101 Firefox/75.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/75.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.10; rv:75.0) Gecko/20100101 Firefox/75.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (X11; Linux; rv:74.0) Gecko/20100101 Firefox/74.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/73.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (X11; OpenBSD i386; rv:72.0) Gecko/20100101 Firefox/72.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:71.0) Gecko/20100101 Firefox/71.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:70.0) Gecko/20191022 Firefox/70.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:70.0) Gecko/20190101 Firefox/70.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows; U; Windows NT 9.1; en-US; rv:12.9.1.11) Gecko/20100821 Firefox/70'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:69.2.1) Gecko/20100101 Firefox/69.2'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; rv:68.7) Gecko/20100101 Firefox/68.7'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19577'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (X11) AppleWebKit/62.41 (KHTML, like Gecko) Edge/17.10859 Safari/452.6'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'},
-{"Connection": "close", 'User-Agent' : 'Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.9200'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
-{"Connection": "close", 'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}] #woah thats a lot of headers
 headerz = [
 {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:77.0) Gecko/20190101 Firefox/77.0'},
 {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0'},
@@ -141,9 +112,7 @@ headerz = [
 {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
 {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
 {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_3_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'},
-{'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
-] #wow even more headers
-header = random.choice(headerz)
+{'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}] #wow even more headers
 Results = [] 
 GoodList = [] 
 Total_Found = 0 
@@ -538,7 +507,7 @@ def parse(): #bossman part of the code
                         ProxyError += 1
                         dorkparse = 0
                 if engine == "Google": #yea dont work
-                    header = random.choice(headerzz)
+                    header = random.choice(headerz)
                     first+=1
                     url = f"https://www.google.com/search?q={urllib.parse.quote(dorklia)}&start={first*10}"
                     try:
@@ -571,7 +540,7 @@ def parse(): #bossman part of the code
                         ProxyError+=1
                         dorkparse = 0
                 if engine == "Yahoo":
-                    header = random.choice(headerzz)
+                    header = random.choice(headerz)
                     first+=1
                     url = f"https://search.yahoo.com/search?p={urllib.parse.quote(dorklia)}&b={first*10}"
                     try:
@@ -903,7 +872,7 @@ def parser2():
     ctypes.windll.kernel32.SetConsoleTitleW(f"Avine By KillinMachine#2570 | Parsed dorks = {Total}  |  Total Found Links = {Total_Found}  |  Duplicates = {Duplicates}  |  Valid Links = {Valid} |  Proxy Error = {ProxyError}  |  Error = {Error}")
     Drok()
     proxy()
-    threading.Thread(target=parse, args=()).start() #idk 
+    threading.Thread(target=parse, args=()).start() #i would just make this a for loop for people who want custom threads maybe in an update in 2050 :D
     threading.Thread(target=parse, args=()).start() 
     threading.Thread(target=parse, args=()).start()
     threading.Thread(target=parse, args=()).start()
